d6e1aa85b1eaffc345c4b836cd17150c58e67e5e
sri <sriveniyerra9@gmail.com>
2018-09-01 18:05:59 +0530

    created

---
+++ src/main/java/com/learntek/devops/myproject/App.java
@@ -0,0 +1,18 @@
+package com.learntek.devops.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "Hello DevOpsWorld!" );
+System.out.println( "Hello DevOpsWorld Diffrence!" );
+System.out.println( "Hello DevOpsWorld Develop branch!" );
+System.out.println( "Hello DevOpsWorld sprint1 branch intermediate changes..! -- From Source TRee--" ); 
+System.out.println( "Git hub remote change 123456" );
+    }
+}
